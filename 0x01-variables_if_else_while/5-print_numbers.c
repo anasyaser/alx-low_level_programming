12c283294aff2909419a3205feda2250b8823c75
anasyaser <anoos649@gmail.com>
2023-03-18 01:09:13 +0200

    print all digits form 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print all single digit numbders of base 10 from 0
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	};
+	printf("\n");
+	return (0);
+
+}
