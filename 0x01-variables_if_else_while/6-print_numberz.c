778c1067180920650f2e6967b15180f88c4dfae7
anasyaser <anoos649@gmail.com>
2023-03-18 01:15:13 +0200

    print all digits from 0 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print all digits from 0 to 9 using putchar
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	};
+	putchar(10);
+	return (0);
+
+}
