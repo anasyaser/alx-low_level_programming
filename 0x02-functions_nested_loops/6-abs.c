4e3130cfd817cb9b3aa37c80de27467a8387787d
anasyaser <anoos649@gmail.com>
2023-03-22 00:01:36 +0200

    return absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_sign - return absolute value
+ * @c: The int to maintain
+ *
+ * Return: absolute value of integer.
+ */
+
+int _abs(int c)
+{
+	return (abs(c));
+}
