3c0e62c28369ac65bf49791b9fbd4c946c2bf985
anasyaser <anoos649@gmail.com>
2023-03-21 17:53:13 +0200

    check if char is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isalpha - check if char is alphabet.
+ * @c: The character to check
+ *
+ * Return: On success 1.
+ * On failure, 0 is returned.
+ */
+
+int _isalpha(int c)
+{
+	return (isalpha(c));
+}
