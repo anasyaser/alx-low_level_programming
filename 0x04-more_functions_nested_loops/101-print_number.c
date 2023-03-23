2964c8cbe12791f96ee09fc0ff6ba9d99acd01c2
anasyaser <anoos649@gmail.com>
2023-03-23 16:45:51 +0200

    print digits putchar only

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_number - print numbers using _putchar only
+ * @n: number to print as string.
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int i = n;
+	int res;
+	int den = 1;
+
+	if (n < 0)
+	{
+		n = -n;
+		i = n;
+		_putchar('-');
+	}
+	while (i > 9)
+	{
+		den *= 10;
+		i /= 10;
+	}
+	while (n > 9)
+	{
+		res = n / den;
+		n -= res * den;
+		den /= 10;
+		_putchar(res + '0');
+	}
+	_putchar(n + '0');
+}
