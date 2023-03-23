930ab5e9878170c461963f2f31b69bd5017f3d85
anasyaser <anoos649@gmail.com>
2023-03-23 13:42:57 +0200

    Print numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Disply numbers
+ *
+ * Return: none
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
