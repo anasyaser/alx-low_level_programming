2013ad0258e6e018df2e406f31aa72c88161eac6
anasyaser <anoos649@gmail.com>
2023-03-30 23:38:06 +0200

    Fix errors on string copy problem

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,20 +6,19 @@
  * _strncpy - copy nth chars from string to another
  * @des: string to copy to it
  * @src: string to copy from
- * @n: n of char to copy 
+ * @n: n of char to copy
  * Return: add string to des.
  */
 
 char *_strncpy(char *des, char *src, int n)
 {
-
 	int i = 0;
 
-	while (i < n)
+	while (i < n && src[i] != '\0')
 	{
 		des[i] = src[i];
 		i++;
 	}
-
+	des[i] = '\0';
 	return (des);
 }
