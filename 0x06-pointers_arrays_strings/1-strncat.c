5ba031b10e80ff3d1f1379d94ada2cfc6df35dc3
anasyaser <anoos649@gmail.com>
2023-03-30 23:14:42 +0200

    Concatenate strings based on n chars

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,18 +6,20 @@
  * _strncat - concatenate two strings based on n char
  * @des: string to concatenate to it
  * @src: string to concatenate
+ * @n: n char to concatenate
  * Return: add string to des.
  */
 
-char *_strncat(char *des, char *src)
+char *_strncat(char *des, char *src, int n)
 {
 	int des_size = strlen(des);
 	int i = 0;
 
-	while (src[i] != '\0')
+	while (src[i] != '\0' && i < n)
 	{
 		des[des_size + i] = src[i];
 		i++;
 	}
 	des[des_size + i] = '\0';
 	return (des);
+}
