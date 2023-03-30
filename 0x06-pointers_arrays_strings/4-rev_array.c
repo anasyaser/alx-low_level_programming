abe4a2c9c70e51902612e0131f85a522b38e7412
anasyaser <anoos649@gmail.com>
2023-03-31 01:10:42 +0200

    Reverse an array elements

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array in place
+ * @a: Pointer to array
+ * @n: Num of array elements
+ * Return: None
+ */
+
+void reverse_array(int *a, int n)
+{
+	int mid = n / 2;
+	int i = 0;
+
+	while (i <= mid)
+	{
+		int tmp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = tmp;
+		i++;
+	}
+}
