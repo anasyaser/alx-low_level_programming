d63213a2b04491a82a2aae466e1915cbd0ed5c2c
anasyaser <anoos649@gmail.com>
2023-04-06 01:31:56 +0200

    Calculate square root recursively

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * sqrt_helper - helper function to sqrt recursion
+ *
+ * @n: number to calculate its square
+ * @i: incremental number to figure out squares
+ * Return: 1 if found else 0
+ */
+
+int sqrt_helper(int n, int i)
+{
+	if (i <= 0)
+		return (-1);
+
+	if (i * i == n)
+		return (i);
+
+	return (sqrt_helper(n, i - 1));
+}
+
+
+/**
+ * _sqrt_recursion - square root of number
+ *
+ * @n: number to calculate its square root
+ * Return: (int) square root of given number
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt_helper(n, n / 2));
+}
