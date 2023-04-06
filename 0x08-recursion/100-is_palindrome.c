7baf1d67622b08bd8935308fb49d939575ad5689
anasyaser <anoos649@gmail.com>
2023-04-06 14:06:18 +0200

    Check is string is palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+
+/**
+ * is_palindrome_helper - helper to check if string is palindrome recursively
+ *
+ * @s: string to check if it's palindrome
+ * @i: incremental number
+ * Return: int (1) if palindrome else (0)
+ */
+
+int is_palindrome_helper(char *s, int i)
+{
+	if (strlen(s) == 0)
+		return (1);
+
+	if (s == (s + strlen(s) - 1 - i))
+		return (1);
+
+	if ((s + 1) == (s + strlen(s) - 1 - i))
+		return (*(s) == *(s + strlen(s) - 1 - i));
+
+	return (*(s) == *(s + strlen(s) - 1 - i) &&
+		is_palindrome_helper((s + 1), (i + 1)));
+}
+
+/**
+ * is_palindrome - check if string is palindrome recursively
+ *
+ * @s: string to check if it's palindrome
+ * Return: int (1) if palindrome else (0)
+ */
+
+int is_palindrome(char *s)
+{
+	return (is_palindrome_helper(s, 0));
+}
