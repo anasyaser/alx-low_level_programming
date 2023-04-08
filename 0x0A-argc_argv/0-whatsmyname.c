095a8a52fc49079ec7a750bcea02892329709af4
anasyaser <anoos649@gmail.com>
2023-04-09 01:30:02 +0200

    print programme path

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print number of arguments
+ *
+ * @argc: number of program arguments
+ * @argv: array of program arguments
+ * Return: (0) Always success
+ */
+
+
+int main( __attribute__((unused))int argc, char *argv[])
+{
+	printf("%s\n", *argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio>
+
+/**
+ * main - print number of arguments
+ *
+ * Return: (0) Always success
+ */
+
+
+int main(int argc, char *argv[])
+{
+	printf("%d", argc - 1);
+
+	return (0);
+}
