5296b731b7bb2cb6c5afb23f3e9c947d959c67f4
anasyaser <anoos649@gmail.com>
2023-04-09 01:43:39 +0200

    Fix betty issue

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,9 +10,9 @@
  */
 
 
-int main( __attribute__((unused))int argc, char *argv[])
+int main(__attribute__((unused))int argc, char *argv[])
 {
-	printf("%s\n", *argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
