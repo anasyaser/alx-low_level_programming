0bf84b6b229a5a2e227080939e77e9daf5cc9690
anasyaser <anoos649@gmail.com>
2023-04-11 17:12:16 +0200

    Create 2D array using malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,9 @@ int _strlen(char *s)
 {
 	int i = 0;
 
+	if (s == NULL)
+		return (0);
+
 	while (*s)
 	{
 		i++;
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+ * alloc_grid - create 2-D array
+ *
+ * @width: width of array
+ * @height: height of array
+ * Return: 2-D array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i = 0;
+
+	if (width *  height <= 0)
+		return (NULL);
+
+	arr = (int **)malloc(height * sizeof(arr));
+
+	if (arr == NULL)
+		return (NULL);
+
+	while (i < height)
+	{
+		arr[i] = malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+		{
+			return (NULL);
+		}
+		i++;
+	}
+
+	return (arr);
+}
