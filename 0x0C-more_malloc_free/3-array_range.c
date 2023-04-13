61bc08e80cc822ace40e3a73af48c75f893f99ae
anasyaser <anoos649@gmail.com>
2023-04-13 17:20:42 +0200

    Create range of numbers

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * array_range - create array of given range
+ *
+ * @min: range start of array
+ * @max: range end of an array
+ * Return: int array of given range
+ */
+
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i;
+
+	if (min > max)
+		return (NULL);
+
+	ptr = malloc((max - min + 1) * sizeof(int));
+	if (ptr == NULL)
+		return (NULL);
+	while (min <= max)
+	{
+		ptr[i] = min;
+		min++;
+		i++;
+	}
+
+	return (ptr);
+}
