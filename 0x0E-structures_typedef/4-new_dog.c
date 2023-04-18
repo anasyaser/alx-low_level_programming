d8cc9bf38a1d7b5f6805d9c2b8f2f9483949d232
anasyaser <anoos649@gmail.com>
2023-04-18 23:49:36 +0200

    Create dog and free

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - create new dog
+ *
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: new dog pointer
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *n_dog;
+
+	n_dog = malloc(sizeof(dog_t));
+	if (n_dog == NULL)
+		return (NULL);
+
+	n_dog->name = name;
+	n_dog->age = age;
+	n_dog->owner = owner;
+
+	return (n_dog);
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - rlease memory allocation from dog
+ *
+ * @d: pointer to dog variable
+ * Return: None
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+}
