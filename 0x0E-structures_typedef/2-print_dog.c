7a252d7aefe85eb3c9c377359cc42db1edba8fb4
anasyaser <anoos649@gmail.com>
2023-04-18 23:11:23 +0200

    print dog attributes

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,10 @@
 
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", d->name);
-	printf("Age: %s\n", d->age);
-	printf("Owner: %s", d->owner);
+	if (!d)
+		return;
+
+	printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+	printf("Age: %.1f\n", d->age);
+	printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 }
