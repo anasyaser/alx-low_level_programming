244f7e9c8e20cc611e5647f826bfe7878f508754
anasyaser <anoos649@gmail.com>
2023-04-18 18:02:15 +0200

    Dog header struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/** struct dog - dog data log
+ * @name: dog name
+ * @age: the age of dog
+ * @owner: the dog owner
+ *
+ * Description: Header of dog attributes for its name, age and owner
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char* owner;
+};
