5b6231fc2b2eae1d00485e84e24e1105961bfcac
anasyaser <anoos649@gmail.com>
2023-04-20 01:53:40 +0200

    Fix 3-calculator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "3-calc.h"
 
 /**
@@ -20,7 +21,7 @@ int (*get_op_func(char *s))(int a, int b)
 	};
 	int i = 0;
 
-	while (ops[i].op)
+	while (ops[i].op && strlen(s) == 1)
 	{
 		if (*s == *(ops[i].op))
 			return (ops[i].f);
