56ff2f127f4774f53cdfabbf23d32e3e25ea48af
anasyaser <anoos649@gmail.com>
2023-04-20 15:24:43 +0200

    1-Print all numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print all argment numbers
+ *
+ * @separator: separator between numbers
+ * @n: number of integers to print
+ * Return: None
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list op;
+
+	if (n == 0)
+		return;
+
+	va_start(op, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(op, int));
+		if (separator)
+			printf("%s", separator);
+	}
+	va_end(op);
+	printf("\n");
+}
