2398e4791e73f70a78357377e8c0e7864c019f17
anasyaser <anoos649@gmail.com>
2023-04-27 17:44:31 +0200

    refix add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,18 +11,16 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	char *re_str;
 	unsigned int len = 0;
 
-	re_str = strdup(str);
-	while (*(re_str + len))
+	while (*(str + len))
 		len++;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = re_str;
+	new_node->str = strdup(str);
 	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
