76042c7c21dc09a6e1e3d4508e3c4b851e6de41d
anasyaser <anoos649@gmail.com>
2023-04-30 00:07:29 +0300

    Fix betty issue

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -24,7 +24,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		*head = new_node;
 		return (new_node);
 	}
-	while(current->next)
+	while (current->next)
 		current = current->next;
 
 	current->next = new_node;
