ae4046578511dfd7819b8428155145e2f5e2096f
anasyaser <anoos649@gmail.com>
2023-04-30 00:04:02 +0300

    add node to the end of SLL

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - add node at the end of list
+ *
+ * @head: pointer to head of elments
+ * @n: node value
+ * Return: pointer to new added value
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+	listint_t *current = *head;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	if (!current)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	while(current->next)
+		current = current->next;
+
+	current->next = new_node;
+	new_node->next = NULL;
+
+	return (new_node);
+}
