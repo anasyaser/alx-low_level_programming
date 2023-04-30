efbed193fb0e48e6bff19492fdfeccef4f546d6e
anasyaser <anoos649@gmail.com>
2023-05-01 00:41:34 +0300

    free listint 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free memory allocation
+ *
+ * @head: pointer to head of list
+ * Return: None
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp = *head;
+
+	if (head == NULL)
+		return;
+	while (tmp)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+}
