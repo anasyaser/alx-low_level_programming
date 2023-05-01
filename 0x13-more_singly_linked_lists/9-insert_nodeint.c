dbd226152e8700bfa7c6132d95321a4201a2f8e4
anasyaser <anoos649@gmail.com>
2023-05-01 23:57:39 +0300

    Fix 5 add node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,8 +15,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *current = *head;
 	listint_t *prev;
 
-	if (!current)
-		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
