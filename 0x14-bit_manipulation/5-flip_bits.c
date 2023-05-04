3381816d77a8c3be4a692c7dc853f8f2e5d7d3b9
anasyaser <anoos649@gmail.com>
2023-05-05 00:49:09 +0300

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - number of bits changed to change given number
+ *
+ * @n: pointer to integer value
+ * @m: number needed to be after chane n
+ * Return: (int) number of changed bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int tmp = n ^ m;
+	unsigned int cnt = 0;
+	int i;
+
+	for (i = 0; i < 32; i++)
+	{
+		if (tmp & (1 << i))
+			cnt++;
+	}
+
+	return (cnt);
+}
