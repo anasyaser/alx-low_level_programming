0ff1f26dce2a9350da3c5ca1a4e24e8014889ff7
anasyaser <anoos649@gmail.com>
2023-05-09 02:38:39 +0300

    Fix copy file content

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,6 +3,8 @@
 /**
  * main - copy from file to anothe
  *
+ * @argc: number of programm arguments
+ * @argv: pointer to list of arguments
  * Return: Always 0.
  */
 
@@ -10,7 +12,8 @@ int main(int argc, char **argv)
 {
 	int fd_from;
 	int fd_to;
-	char cur;
+	char cur[1024];
+	ssize_t flag = 0;
 
 	if (argc != 3)
 	{
@@ -19,9 +22,8 @@ int main(int argc, char **argv)
 		exit(97);
 	}
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 00664);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
 	fd_from = open(argv[1], O_RDONLY);
-
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
@@ -29,23 +31,23 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	while (read(fd_from, &cur, 1))
+	flag = read(fd_from, cur, 1024);
+	while (flag)
 	{
-		if (fd_to == -1 || (write(fd_to, &cur, 1)) == -1)
+		if (fd_to == -1 || (write(fd_to, cur, flag)) == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
 				argv[2]);
 			exit(99);
 		}
+		flag = read(fd_from, cur, 1024);
 	}
+
 	if (close(fd_to))
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd_to);
-	}
 	if (close(fd_from))
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd_from);
-	}
+
 	return (0);
 }
 
