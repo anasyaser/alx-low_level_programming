109c49f7bbcf66743a43b7de0feb9306277c4b3f
anasyaser <anoos649@gmail.com>
2023-05-09 01:40:10 +0300

    Copy file content to another

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,7 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
+	/* need to modified to handle if read or write return -1 or error*/
 	while (letters && read(fd, &cur, 1))
 	{
 		number += write(STDOUT_FILENO, &cur, 1);
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - copy from file to anothe
  *
  * Return: Always 0.
  */
@@ -10,23 +10,42 @@ int main(int argc, char **argv)
 {
 	int fd_from;
 	int fd_to;
+	char cur;
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n",
+			argv[0]);
 		exit(97);
 	}
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 00664);
 	fd_from = open(argv[1], O_RDONLY);
 
 	if (fd_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			argv[1]);
 		exit(98);
 	}
 
-	
+	while (read(fd_from, &cur, 1))
+	{
+		if (fd_to == -1 || (write(fd_to, &cur, 1)) == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+				argv[2]);
+			exit(99);
+		}
+	}
+	if (close(fd_to))
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd_to);
+	}
+	if (close(fd_from))
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd_from);
+	}
 	return (0);
 }
 
