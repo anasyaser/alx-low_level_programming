8111639ce6eb1290931c3246a75e577188c70719
anasyaser <anoos649@gmail.com>
2023-05-08 23:22:52 +0300

    Fix betty issue

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,12 +19,13 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	while(*text_content)
+	if (text_content == NULL)
+		return (1);
+	while (*text_content)
 	{
-		if(write(fd, text_content, 1) == -1)
+		if (write(fd, text_content, 1) == -1)
 			return (-1);
 		text_content++;
 	}
-
 	return (1);
 }
