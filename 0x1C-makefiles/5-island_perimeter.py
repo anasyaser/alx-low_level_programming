3496ff693f7340794716543a1b47192f43b0142e
anasyaser <anoos649@gmail.com>
2023-08-24 06:07:07 +0300

    Fix advanced

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,20 +7,9 @@ def island_perimeter(grid):
         for c in range(len(grid[r])):
             if grid[r][c] == 1:
                 sur = get_surround(r, c, grid)
-                perimeter += sum([1 for w in sur if sur == 0])
+                perimeter += sum([1 for w in sur if w == 0])
     return perimeter
 
 
 def get_surround(i, j, grid):
     return (grid[i][j + 1], grid[j][j - 1], grid[i - 1][j], grid[i + 1][j])
-
-
-if __name__ == "__main__":
-    grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
-    print(island_perimeter(grid))
