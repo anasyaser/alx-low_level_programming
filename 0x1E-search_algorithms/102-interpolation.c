f786ffa3c63c88015c0b88ed67cedab94d4a587c
anasyaser <anoos649@gmail.com>
2024-01-17 11:23:40 +0200

    Interpolation and exponential search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,35 +1,44 @@
-#include <stdio.h>
-#include <math.h>
+#include "search_algos.h"
 
 /**
- * print_array - print arrray of integers
+ * interpolation_search - Interpolation Search Algorithm
  *
- * @array: pointer to an array to print
- * @from: index to print from
- * @to: index to stop print at
- * Return: None
- */
-
-void print_array(int *array, size_t from, size_t to)
-{
-	while (from < to)
-	{
-		printf("%d,", array[from]);
-		from++;
-	}
-	printf("%d\n", array[from]);
-}
-
-/**
- * interpolation_search - interpolation search algorithm
- *
- * @array: pointer to array of integers to search in
+ * @array: pointer to array of integers
  * @size: number of elements in array
- * @value: value to search for in array
- * Return: first index where value is located
+ * @value: value to search for
+ * Return: Index of serched value in array else -1
  */
 
 int interpolation_search(int *array, size_t size, int value)
 {
-	
+	size_t l;
+	size_t h;
+	size_t pos;
+
+	l = 0;
+	h = size - 1;
+
+	if (array == NULL)
+		return (-1);
+	while (array[l] != array[h] && l != h)
+	{
+		pos = l + (((double)(h - l) /
+			    (array[h] - array[l])) * (value - array[l]));
+		if (pos > h || pos < l)
+		{
+			printf("Value checked array[%lu] is out of range\n",
+			       pos);
+			return (-1);
+		}
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		if (value > array[pos])
+			l = pos + 1;
+		else if (value < array[pos])
+			h = pos - 1;
+		else
+			return (pos);
+	}
+	if (value == array[l])
+		return (l);
+	return (-1);
 }
